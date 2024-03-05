28d86e5c640fc93d57e1471e48c18cf5eb494587
Jordan Leigh <151576341+JordannLeigh@users.noreply.github.com>
2024-03-05 19:30:16 +0000

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,14 +1,12 @@
-"""
-
-git init
-git status
-track new file
-Output "Hello World!"
-Change output to "Git is Awesome!"
-
-stop
-
-"""
-
 #print("Hello World!")
-print("Git is Awesome!")
+#print("Git is Awesome!")
+
+message = "Git is Awesome!"
+new_message = ""
+for i, char in enumerate(message):
+    if i % 2 == 0:
+        new_message += char.upper()
+    else:
+        new_message += char.lower()
+print(new_message)
+#output = GiT iS aWeSoMe!
